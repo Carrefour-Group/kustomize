7d4a403578e5f1cae48a882d9897460f7a84cab1
Hamdi MAKNI <hamdi_makni@carrefour.com>
2021-05-19 20:22:58 +0200

    kustomise build: add new flag enable-git-branches-ref to enable referencing branches git ref

--- kustomize/commands/build/build.go
+++ kustomize/commands/build/build.go
@@ -24,7 +24,8 @@ var theFlags struct {
 		plugins        bool
 		managedByLabel bool
 		helm           bool
-		gitCache	   bool
+		gitCache       bool
+		gitRanchesRef  bool
 	}
 	helmCommand    string
 	loadRestrictor string
@@ -107,6 +108,7 @@ func NewCmdBuild(
 	AddFlagEnableManagedbyLabel(cmd.Flags())
 	AddFlagEnableHelm(cmd.Flags())
 	AddFlagEnableGitCache(cmd.Flags())
+	AddFlagEnableGitBranchesRef(cmd.Flags())
 	return cmd
 }
 
@@ -143,5 +145,6 @@ func HonorKustomizeFlags(kOpts *krusty.Options) *krusty.Options {
 	kOpts.PluginConfig.HelmConfig.Command = theFlags.helmCommand
 	kOpts.AddManagedbyLabel = isManagedByLabelEnabled()
 	kOpts.EnableGitCache = theFlags.enable.gitCache
+	kOpts.EnableGitBranchesRef = theFlags.enable.gitRanchesRef
 	return kOpts
 }
